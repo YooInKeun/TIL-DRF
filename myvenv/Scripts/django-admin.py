9f07dce3159cb851235a492061f6709523f000af
YouuInKeun <keun0390@naver.com>
2019-09-09 19:49:47 +0900

    [ADD] DRF 뼈대(Post Model)

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\keun0\onedrive\바탕 화면\멋사 7기\drf study\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
