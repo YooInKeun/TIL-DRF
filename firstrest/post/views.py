d3f460c9c2c943c63b270d14c2f8e1911a3c9e08
YouuInKeun <keun0390@naver.com>
2019-10-29 17:14:52 +0900

    [ADD] Likelion DRF Assignment

--- firstrest/post/views.py
+++ firstrest/post/views.py
@@ -4,7 +4,6 @@ from rest_framework import viewsets
 from .models import Post
 from .serializer import PostSerializer
 
-# CVB > Class로 view 작성
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
